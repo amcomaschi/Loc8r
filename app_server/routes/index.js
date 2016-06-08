48be9f666a00cad79d87f904059d1af2c2f97da3
amcomaschi <arielcomaschi@gmail.com>
2016-06-08 13:57:46 -0300

    Se suben cambios capitulo 3.

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,10 +3,6 @@ var router = express.Router();
 var ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', homepageController});
-
-homepageController = function (req, res) {
-  res.render('index' { title: 'Express'});
-}
+router.get('/', ctrlMain.index);
 
 module.exports = router;
