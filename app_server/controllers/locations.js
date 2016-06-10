68bcfd9eef7d2f7ea9b3b81e8144819f45cdaeeb
amcomaschi <arielcomaschi@gmail.com>
2016-06-10 01:10:31 -0300

    Se suben cambios capitulo 3.

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,18 @@
+/**
+ * Created by amcomaschi on 09/06/16.
+ */
+
+/* GET 'home' page*/
+module.exports.homelist = function (req, res) {
+    res.render('locations-list', { title: 'Home' });
+};
+
+/* GET 'Location info' page*/
+module.exports.locationInfo = function (req, res) {
+    res.render('location-info', { title: 'Location Info'});
+};
+
+/* GET 'Add review' page */
+module.exports.addReview = function (req, res) {
+    res.render('index', { title: 'Add review'});
+};
---
+++ app_server/controllers/others.js
@@ -0,0 +1,8 @@
+/**
+ * Created by amcomaschi on 09/06/16.
+ */
+
+/* GET 'about' page */
+module.exports.about = function (req, res) {
+    res.render('index', { title: 'About' });
+};
