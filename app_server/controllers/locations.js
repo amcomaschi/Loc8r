ea535fde11d06d0e89c0c11c5eb28122d3033284
amcomaschi <arielcomaschi@gmail.com>
2016-06-13 18:00:10 -0300

    Se actualiza cordenadas de Domenica.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -4,12 +4,53 @@
 
 /* GET 'home' page*/
 module.exports.homelist = function (req, res) {
-    res.render('locations-list', { title: 'Home' });
+    res.render('locations-list', {  title: 'Loc8r - Encuentra un lugar con wifi para trabajar',
+                                    pageHeader: {
+                                        title: "Loc8r",
+                                        strapline: "Encuentra lugares con wifi cerca tuyo para terminar tu trabajo !"
+                                    },
+                                    sidebar: "Buscar un buen lugar para almorzar y tomar un trago, venite a Domenica. Contamos con una " +
+                                              "conexion super rapida.",
+                                    locations: [{
+                                        name: "Domenica",
+                                        address: "San Juan 2552, Capital Federal",
+                                        rating: 3,
+                                        facilities: ['Tragos', 'Pizzas', 'Wifi'],
+                                        distance: '100m'
+                                    },{
+                                        name: "Pausa",
+                                        address: "Jujuy 1245, Capital Federal",
+                                        rating: 2,
+                                        facilities: ['Cervezas', 'Minutas', 'Wifi'],
+                                        distance: '90m'
+                                    },{
+                                        name: "Burguer King",
+                                        address: "San Juan 2340, Capital Federal",
+                                        rating: 4,
+                                        facilities: ['Hamburguesas', 'Wifi'],
+                                        distance: '250m'
+                                    }]
+    });
 };
 
 /* GET 'Location info' page*/
 module.exports.locationInfo = function (req, res) {
-    res.render('location-info', { title: 'Location Info'});
+    res.render('location-info', { title: 'Domenica',
+                                  pageHeader: {title: 'Domenica'},
+                                  sidebar: 'dddddddddddddddddd',
+                                  location: {
+                                    name: 'Domenica',
+                                    address: 'San Juan 2552, Capital Federal',
+                                    rating: 4,
+                                    facilities: ['Cerveza', 'Pizzas', 'Wifi'],
+                                    coords: {lat: -34.623832, long: -58.4036347},
+                                    openingTimes: ['Lunes a Viernes : 09:00 am - 11:00 pm', 'Sabados : 10:00 am - 11:00 pm', 'Domingos : cerrado'],
+                                    reviews: [{author: 'Adrian Kamycki',
+                                              rating: 3,
+                                              timestap: '10/05/2016',
+                                              reviewText: 'Que buen lugar !'}]
+                                  }
+                                });
 };
 
 /* GET 'Add review' page */
