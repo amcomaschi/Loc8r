d89bceaa56d1ae8939088e7d6ea9d264b2e3c916
amcomaschi <arielcomaschi@gmail.com>
2016-06-15 16:14:19 -0300

    Se actualiza por capitulo 5, mongoose.

---
+++ app_server/models/locations.js
@@ -0,0 +1,30 @@
+/**
+ * Created by amcomaschi on 15/06/16.
+ */
+var mongoose = require('mongoose');
+
+var openingTimeSchema = new mongoose.Schema({
+	days: { type: String, required: true },
+	opening: { type: String },
+	closing: { type: String },
+	closed: { type: Boolean, required: true }
+});
+
+var reviewSchema = new mongoose.Schema({
+	author: { type: String },
+	rating: { type: String, required: true, min: 0, max: 5 },
+	reviewText: { type: String },
+	createdOn: { type: Date, "default": Date.now()}
+});
+
+var locationSchema = new mongoose.Schema({
+	name: { type: String, required: true },
+	address: { type: String },
+	rating: { type: Number, "default": 0, min: 0, max: 5 },
+	facilities: { type: [String]},
+	coords: { type: [Number], index: '2dsphere'},
+	openingTimes: { type: [openingTimeSchema]},
+	reviews: { type: [reviewSchema]}
+});
+
+mongoose.model('Location', locationSchema);
